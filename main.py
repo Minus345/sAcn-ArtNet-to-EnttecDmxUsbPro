5d7ae92c7b0592c3d7c205d85f342efcfd06a66a
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-05-12 20:55:18 +0200

    add linux support

--- main.py
+++ main.py
@@ -13,56 +13,56 @@ print('Argument List:', str(sys.argv))
 receiver = sacn.sACNreceiver()
 receiver.start()  # start the receiving thread
 
-    if platform == "linux" or platform == "linux2":
-        # linux
-        print("linux")
-        dmx = Controller(sys.argv[1], dmx_size=512)
-    elif platform == "win32":
-        # Windows...
-        print("windows")
-        dmx = Controller(sys.argv[1], dmx_size=512)
+if platform == "win32":
+    # Windows...
+    print("windows")
+    dmx = Controller(sys.argv[1], dmx_size=512)
+if platform == "linux" or platform == "linux2":
+    # linux
+    print("linux")
+    dmx = Controller(sys.argv[1], dmx_size=512)
 
-    # my_port = get_port_by_serial_number('EN160150A')
-    # my_port = get_port_by_product_id(24577)
-    # dmx = Controller(my_port, dmx_size=512)
+# my_port = get_port_by_serial_number('EN160150A')
+# my_port = get_port_by_product_id(24577)
+# dmx = Controller(my_port, dmx_size=512)
 
-    dmx.set_dmx_parameters(output_rate=0)
+dmx.set_dmx_parameters(output_rate=0)
 
-    if not sys.argv[2].isnumeric():
-        print("false argument")
-        receiver.stop()
-        sys.exit(0)
+if not sys.argv[2].isnumeric():
+    print("false argument")
+    receiver.stop()
+    sys.exit(0)
 
-    universe: int = int(sys.argv[2])
-    print(universe)
+universe: int = int(sys.argv[2])
+print(universe)
 
-    dmxPacket = [0] * 512
+dmxPacket = [0] * 512
 
-    receiver.join_multicast(universe)
-
-
-    @receiver.listen_on('universe', universe=universe)  # listens on universe 1
-    def callback(packet):  # packet type: sacn.DataPacket
-        counter: int = 0
-        for x in packet.dmxData:
-            dmxPacket[counter] = x
-            counter = counter + 1
-            # print(dmxPacket)
+receiver.join_multicast(universe)
 
 
-    def handler(signum, frame):
-        receiver.stop()
-        exit(1)
+@receiver.listen_on('universe', universe=universe)  # listens on universe 1
+def callback(packet):  # packet type: sacn.DataPacket
+    counter: int = 0
+    for x in packet.dmxData:
+        dmxPacket[counter] = x
+        counter = counter + 1
+        # print(dmxPacket)
 
 
-    signal.signal(signal.SIGINT, handler)
+def handler(signum, frame):
+    receiver.stop()
+    exit(1)
 
-    while True:
-        counter: int = 1
-        for x in dmxPacket:
-            dmx.set_channel(counter, x)
-            counter = counter + 1
-        dmx.submit()
+
+signal.signal(signal.SIGINT, handler)
+
+while True:
+    counter: int = 1
+    for x in dmxPacket:
+        dmx.set_channel(counter, x)
+        counter = counter + 1
+    dmx.submit()
 
     # dmx.set_channel(1, 255)  # Sets DMX channel 1 to max 255
     # dmx.submit()  # Sends the update to the controller
