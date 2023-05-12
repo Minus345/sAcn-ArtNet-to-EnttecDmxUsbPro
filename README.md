51159b1a677f84d1bd75d7bf58b2a2255d2d06e5
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-05-12 20:35:47 +0200

    add linux support

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-# sAcn_to_EnttecDmxUsbPro
+# sAcn to EnttecDmxUsbPro
+
 sAcn to EnttecDmxUsbPro phyton script
 
-used librarys:
+Tipp:
+`python -m DMXEnttecPro.utils`  
+find usb Port
+
+## Used librarys:
+
 - `pip install sacn`
 - `pip install DMXEnttecPro`
+- `pip install pyserial`
+
+## Parameters:
+
+- **usbPort:**
+    - linux: `COM4`
+    - windows: `/dev/ttyUSB0`
+- **sAcn Universe**
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 from DMXEnttecPro import Controller
 from DMXEnttecPro.utils import get_port_by_serial_number, get_port_by_product_id
+from sys import platform
 
 print("starting")
 print('Number of arguments:', len(sys.argv), 'arguments.')
@@ -12,51 +13,60 @@ print('Argument List:', str(sys.argv))
 receiver = sacn.sACNreceiver()
 receiver.start()  # start the receiving thread
 
-my_port = get_port_by_serial_number('EN160150A')
-my_port = get_port_by_product_id(24577)
-dmx = Controller(my_port)
+if platform == "linux" or platform == "linux2":
+    # linux
+    print("linux")
+    dmx = Controller(sys.argv[1], dmx_size=512)
+elif platform == "win32":
+    # Windows...
+    print("windows")
+    dmx = Controller(sys.argv[1], dmx_size=512)
 
-# dmx = Controller('COM8')  # Typical of Windows
-# dmx = Controller('/dev/ttyUSB0')  # Typical of Linux
-if not sys.argv[1].isnumeric():
-    print("false argument")
-    receiver.stop()
-    sys.exit(0)
+    # my_port = get_port_by_serial_number('EN160150A')
+    # my_port = get_port_by_product_id(24577)
+    # dmx = Controller(my_port, dmx_size=512)
 
-universe: int = int(sys.argv[1])
-print(universe)
+    dmx.set_dmx_parameters(output_rate=0)
 
-dmxPacket = [0] * 512
+    if not sys.argv[2].isnumeric():
+        print("false argument")
+        receiver.stop()
+        sys.exit(0)
 
-receiver.join_multicast(universe)
+    universe: int = int(sys.argv[2])
+    print(universe)
 
+    dmxPacket = [0] * 512
 
-@receiver.listen_on('universe', universe=universe)  # listens on universe 1
-def callback(packet):  # packet type: sacn.DataPacket
-    counter: int = 0
-    for x in packet.dmxData:
-        dmxPacket[counter] = x
-        counter = counter + 1
-        # print(dmxPacket)
-
-
-def handler(signum, frame):
-    receiver.stop()
-    exit(1)
+    receiver.join_multicast(universe)
 
 
-signal.signal(signal.SIGINT, handler)
+    @receiver.listen_on('universe', universe=universe)  # listens on universe 1
+    def callback(packet):  # packet type: sacn.DataPacket
+        counter: int = 0
+        for x in packet.dmxData:
+            dmxPacket[counter] = x
+            counter = counter + 1
+            # print(dmxPacket)
 
-while True:
-    counter: int = 1
-    for x in dmxPacket:
-        dmx.set_channel(counter, x)
-        counter = counter + 1
-    dmx.submit()
 
-# dmx.set_channel(1, 255)  # Sets DMX channel 1 to max 255
-# dmx.submit()  # Sends the update to the controller
-# time.sleep(1)
-# dmx.set_channel(1, 0)  # Sets DMX channel 1 to max 255
-# dmx.submit()  # Sends the update to the controller
-# time.sleep(1)
+    def handler(signum, frame):
+        receiver.stop()
+        exit(1)
+
+
+    signal.signal(signal.SIGINT, handler)
+
+    while True:
+        counter: int = 1
+        for x in dmxPacket:
+            dmx.set_channel(counter, x)
+            counter = counter + 1
+        dmx.submit()
+
+    # dmx.set_channel(1, 255)  # Sets DMX channel 1 to max 255
+    # dmx.submit()  # Sends the update to the controller
+    # time.sleep(1)
+    # dmx.set_channel(1, 0)  # Sets DMX channel 1 to max 255
+    # dmx.submit()  # Sends the update to the controller
+    # time.sleep(1)
