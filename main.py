6eb6e07ab5b3e23e34389c06cc55f9b48d3766c3
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-05-14 19:32:21 +0200

    bugfix

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ from stupidArtnet import StupidArtnetServer
 print("starting")
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
-receiver = sacn.sACNreceiver()
-receiver.start()
 mode = sys.argv[3]
 if mode == "artnet" or mode == "sacn":
     print(mode)
 else:
     print("use artnet or sacn")
-    receiver.stop()
     exit(1)
 
 if platform == "win32":
@@ -33,6 +30,7 @@ dmx.set_dmx_parameters(output_rate=0)
 dmxPacket = [0] * 512
 
 
+# ArtNet
 def test_callback(data):
     counter1: int = 0
     for x in data:
@@ -40,6 +38,18 @@ def test_callback(data):
         counter1 = counter1 + 1
 
 
+if mode == "sacn":
+    receiver = sacn.sACNreceiver()
+    receiver.start()
+    receiver.join_multicast(universe)
+
+if mode == "artnet":
+    a = StupidArtnetServer()
+    u1_listener = a.register_listener(
+        universe, callback_function=test_callback)
+
+
+# sacn
 @receiver.listen_on('universe', universe=universe)  # listens on universe 1
 def callback(packet):  # packet type: sacn.DataPacket
     counter2: int = 0
@@ -55,14 +65,7 @@ def handler(signum, frame):
 
 signal.signal(signal.SIGINT, handler)
 
-if mode == "sacn":
-    receiver.join_multicast(universe)
-
-if mode == "artnet":
-    a = StupidArtnetServer()
-    u1_listener = a.register_listener(
-        universe, callback_function=test_callback)
-
+# main loop
 while True:
     counter: int = 1
     for x in dmxPacket:
